72e1d557e67091b2e3ab0f8e1765b35fdac58e28
Eric <xeric.hox@gmail.com>
2020-06-27 23:29:09 -0700

    added blur

--- js/main.js
+++ js/main.js
@@ -232,6 +232,7 @@ jQuery(document).ready(function($) {
   		var st = $(this).scrollTop();
 
   		if (st > 100) {
+
   			$('.js-sticky-header').addClass('shrink');
   		} else {
   			$('.js-sticky-header').removeClass('shrink');
@@ -241,34 +242,6 @@ jQuery(document).ready(function($) {
 
   };
   siteScroll();
-
-
-  var siteIstotope = function() {
-  	/* activate jquery isotope */
-	  var $container = $('#posts').isotope({
-	    itemSelector : '.item',
-	    isFitWidth: true
-	  });
-
-	  $(window).resize(function(){
-	    $container.isotope({
-	      columnWidth: '.col-sm-3'
-	    });
-	  });
-	  
-	  $container.isotope({ filter: '*' });
-
-	    // filter items on button click
-	  $('#filters').on( 'click', 'button', function(e) {
-	  	e.preventDefault();
-	    var filterValue = $(this).attr('data-filter');
-	    $container.isotope({ filter: filterValue });
-	    $('#filters button').removeClass('active');
-	    $(this).addClass('active');
-	  });
-  }
-
-  siteIstotope();
 
 
   $('.fancybox').on('click', function() {
